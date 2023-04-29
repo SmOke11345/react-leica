2f3c2df7c1c2cf2553228071fa63d005a3ba2350
SmOke11345 <103870835+SmOke11345@users.noreply.github.com>
2023-04-29 22:26:08 +0600

    add ReactRemoveScroll for burger overlay

--- src/components/header/Burger.jsx
+++ src/components/header/Burger.jsx
@@ -1,29 +1,31 @@
-import style from './style.module.css';
 import React from 'react';
+import ReactRemoveScroll from 'react-remove-scroll/dist/es2019/Combination';
+import style from './style.module.css';
 import burgerClose from './img/burger_close.svg';
 
 export const Burger = ({ isOpen, setIsOpen }) => {
     return (
-        <div className={`${style.overlay} ${isOpen ? `${style.show}` : ''}`}>
-            <img
-                src={burgerClose}
-                alt="burger_menu"
-                className={style.burger}
-                onClick={(event) => {
-                    setIsOpen(!isOpen);
-                    console.log(isOpen);
-                }}
-            />
-            <div className={style.content_wrapper}>
-                <nav className={style.nav}>
-                    <div className={style.nav__item}>
-                        <a href="/">Преимущества</a>
-                    </div>
-                    <div className={style.nav__item}>
-                        <a href="/">Галерея</a>
-                    </div>
-                </nav>
+        <ReactRemoveScroll>
+            <div className={`${style.overlay} ${isOpen ? `${style.show}` : ''}`}>
+                <img
+                    src={burgerClose}
+                    alt="burger_menu"
+                    className={style.burger}
+                    onClick={(event) => {
+                        setIsOpen(!isOpen);
+                    }}
+                />
+                <div className={style.content_wrapper}>
+                    <nav className={style.nav}>
+                        <div className={style.nav__item}>
+                            <a href="/">Преимущества</a>
+                        </div>
+                        <div className={style.nav__item}>
+                            <a href="/">Галерея</a>
+                        </div>
+                    </nav>
+                </div>
             </div>
-        </div>
+        </ReactRemoveScroll>
     );
 };
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -25,7 +25,6 @@ export const Header = () => {
                         className={style.burger}
                         onClick={(event) => {
                             setIsOpen(!isOpen);
-                            console.log(isOpen);
                         }}
                     />
                     <Burger isOpen={isOpen} setIsOpen={setIsOpen} />
